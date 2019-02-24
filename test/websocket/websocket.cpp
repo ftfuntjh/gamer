fac2d8c86c01b4db618fea13913aa61bfee11d69
caogaoyang <imallrightgg@gmail.com>
2019-02-25 03:55:29 +0800

    fix test _main symbol not found

--- test/websocket/websocket.cpp
+++ test/websocket/websocket.cpp
@@ -2,8 +2,17 @@
 // Created by 曹高阳 on 2019-02-25.
 //
 #include <websocket/websocket.h>
+#include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
 TEST(GAMER_WEBSOCKET, websocket_init_test) {
-    gamer::websocket w{std::string{"test.com"}, std::string{"/test"}};
+    gamer::websocket w{std::string("test"), std::string("/test")};
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+
+    int returnValue = RUN_ALL_TESTS();
+
+    return returnValue;
 }
