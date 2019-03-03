345093c8251ba85359f06c888620d54f12915620
ftfunjth <768181755@qq.com>
2019-03-03 22:38:41 +0800

    init...

--- src/websocket/request_context.cpp
+++ src/websocket/request_context.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "request_context.h"
+
+namespace gamer {
+    namespace websocket {
+        request_context::request_context() {
+            //todo add constructor logic
+        }
+    }
+}
